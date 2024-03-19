8a7f8e48e2db535c451d9d84f049a4b6fa26e166
Francisco Daniel Freitas Martins <francisco.daniel@ambevtech.com.br>
2024-03-19 20:40:39 -0300

    adding repository interfaces

---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchMvc.Domain.Enitities;
+
+namespace CleanArchMvc.Domain.Interfaces;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetProductsAsync();
+    Task<Product> GetByIdAsync(int? id);
+    Task<Product> GetProductCategoryAsync(int? id);
+    Task<Category> CreateAsync(Product category);
+    Task<Category> UpdateAsync(Product category);
+    Task<Category> RemoveAsync(Product category);
+}
--- CleanArchMvc.Domain/obj/Debug/net8.0/CleanArchMvc.Domain.AssemblyInfo.cs
+++ CleanArchMvc.Domain/obj/Debug/net8.0/CleanArchMvc.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceac02e06d72359c5e11ac33b760a952a1c04f5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09016c182a6f8ef226c5002f0d3ab0fa08d3ed7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
